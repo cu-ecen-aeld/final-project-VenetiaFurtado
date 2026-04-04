c15d03db02c80d0c7ca8ee50cfbd87f9069c5313
Venetia Furtado <venetiarosita@gmail.com>
2026-04-04 19:07:21 +0000

    enabled i2c module for hardware

--- shared.sh
+++ shared.sh
@@ -5,7 +5,9 @@
 RPI4_DEFCONFIG=configs/raspberrypi4_64_defconfig
 # The place we store customizations to the qemu configuration
 MODIFIED_QEMU_DEFCONFIG=base_external/configs/aesd_qemu_defconfig
+#The place we store customizations to the RPi configuration
+MODIFIED_RPI_DEFCONFIG=base_external/configs/aesd_rpi_defconfig
 # The defconfig from the buildroot directory we use for the project
 AESD_DEFAULT_DEFCONFIG=${RPI4_DEFCONFIG}
-AESD_MODIFIED_DEFCONFIG=${MODIFIED_QEMU_DEFCONFIG}
+AESD_MODIFIED_DEFCONFIG=${MODIFIED_RPI_DEFCONFIG}
 AESD_MODIFIED_DEFCONFIG_REL_BUILDROOT=../${AESD_MODIFIED_DEFCONFIG}
