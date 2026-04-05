4ee2add40ba5048b43930646ba0d2eadb4354e1d
Venetia Furtado <venetiarosita@gmail.com>
2026-04-05 22:55:08 +0000

    sensor load/unload removed

--- base_external/package/bme280_sensor/sensor.mk
+++ base_external/package/bme280_sensor/sensor.mk
@@ -7,7 +7,7 @@
 #
 ##############################################################
 
-BME280_SENSOR_VERSION = '10441a83e55e3115036a6bf9bcffb0dcc750f686'
+BME280_SENSOR_VERSION = '83f7c3f84bf26d2160df22d49cd23073aad47a7a'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -16,12 +16,6 @@ BME280_SENSOR_SITE_METHOD = git
 BME280_SENSOR_GIT_SUBMODULES = YES
 
 BME280_SENSOR_MODULE_SUBDIRS = bme280_sensor
-
-define BME280_SENSOR_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/bme280_sensor/bme280_sensor_load $(TARGET_DIR)/usr/bin/
-	$(INSTALL) -m 0755 $(@D)/bme280_sensor/bme280_sensor_unload $(TARGET_DIR)/usr/bin/
-
-endef
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
