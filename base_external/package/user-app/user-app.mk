3e0503b2d7375ee9a2421bff2e5bae5bfaafb749
Venetia Furtado <venetiarosita@gmail.com>
2026-04-18 02:04:57 +0000

    added user app support

--- base_external/package/user-app/user-app.mk
+++ base_external/package/user-app/user-app.mk
@@ -7,15 +7,22 @@
 #
 ##############################################################
 
-BME280_SENSOR_VERSION = '0dcb8ac592a7a60f1c220a5494a5bad066481fa5'
+USER_APP_VERSION = '0dcb8ac592a7a60f1c220a5494a5bad066481fa5'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-BME280_SENSOR_SITE = 'git@github.com:VenetiaFurtado/Multi_Sensor_Event_Driver.git'
-BME280_SENSOR_SITE_METHOD = git
-BME280_SENSOR_GIT_SUBMODULES = YES
+USER_APP_SITE = 'git@github.com:VenetiaFurtado/Multi_Sensor_Event_Driver.git'
+USER_APP_SITE_METHOD = git
+USER_APP_GIT_SUBMODULES = YES
 
-BME280_SENSOR_MODULE_SUBDIRS = bme280_sensor
+define USER_APP_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/user-app all
+endef
 
-$(eval $(kernel-module))
+define USER_APP_INSTALL_TARGET_CMDS
+
+	$(INSTALL) -m 0755 $(@D)/user-app/user-app $(TARGET_DIR)/usr/bin/
+
+endef
+
 $(eval $(generic-package))
